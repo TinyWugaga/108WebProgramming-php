c65a7e7b69a9e2b016aaa994a67161f76bdc0774
Tiny <405401372@gapp.fju.edu.tw>
2019-11-10 01:54:17 +0800

    add lecture folder

---
+++ week10/lecture/db.php
@@ -0,0 +1,21 @@
+<?php
+  $servername = "localhost";
+  $dbname = "practice";
+  $username = "root";
+  $password = "12345678";
+
+
+  try
+  { // 初始化 PDO 對象$conn
+
+    //$dsn = “數據庫類型 :  主機地址;  資料庫名” ,  連接用戶, 用戶密碼
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;", $username, $password);
+    //PDO 設置屬性（這邊設定的是  錯誤報告, 拋出 exceptions 異常）
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully"; //PDO 連接成功印出訊息
+  }
+  catch(PDOException $e)
+  {//接取 PDO 拋出錯誤
+    echo "Connection failed: " . $e->getMessage(); //PDO 連接失敗印出訊息，並拋出錯誤內容
+  }
